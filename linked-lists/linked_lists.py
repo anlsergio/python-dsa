75838977752cb1bbcbf26e7127cece2ce3df88d6
Anderson L. Sergio <asergio@wiley.com>
2024-09-17 08:28:55 -0300

    Replace linked list exercise code

--- linked-lists/linked_lists.py
+++ linked-lists/linked_lists.py
@@ -1,88 +1,202 @@
 class Node:
-    def __init__(self, value, next_node=None):
+    def __init__(self, value):
         self.value = value
-        self.next_node = next_node
-
-    def get_value(self):
-        return self.value
-
-    def get_next_node(self):
-        return self.next_node
-
-    def set_next_node(self, next_node):
-        self.next_node = next_node
-
+        self.next = None
 
 class LinkedList:
-    def __init__(self, value=None):
-        self.head_node = Node(value)
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
-    def get_head_node(self):
-        return self.head_node
+    def print(self):
+        temp = self.head
+        if not temp:
+            print("List is empty!")
+            return
+        while temp:
+            print("value: "+ str(temp.value) + " next: "+ str(temp.next))
+            temp = temp.next
 
-    def insert_beginning(self, new_value):
-        new_node = Node(new_value)
-        new_node.set_next_node(self.head_node)
-        self.head_node = new_node
-
-    def stringify_list(self):
-        string_list = ""
-        current_node = self.get_head_node()
-        while current_node:
-            if current_node.get_value():
-                string_list += str(current_node.get_value()) + "\n"
-            current_node = current_node.get_next_node()
-        return string_list
-
-    def remove_node(self, value_to_remove):
-        current_node = self.get_head_node()
-        if current_node.get_value() == value_to_remove:
-            self.head_node = current_node.get_next_node()
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
         else:
-            while current_node:
-                next_node = current_node.get_next_node()
-                if next_node.get_value() == value_to_remove:
-                    current_node.set_next_node(next_node.get_next_node())
-                    current_node = None
-                else:
-                    current_node = next_node
+            self.head = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
-    def remove_all_matching_nodes(self, value_to_remove):
-        # Remove matching head nodes
-        while self.head_node and self.head_node.get_value() == value_to_remove:
-            self.head_node = self.head_node.get_next_node()
+    def append(self, value):
+        new_node = Node(value)
+        if self.head:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+        self.length += 1
 
-        # initialize the current node variable
-        # with the head node.
-        current_node = self.get_head_node()
+    def pop(self):
+        if not self.head:
+            return None
+        current_node = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return current_node
+        previous_node = self.head
+        while True:
+            is_tail = current_node.next is None
+            if is_tail:
+                previous_node.next = None
+                self.tail = previous_node
+                self.length -= 1
+                return current_node
 
-        # Traverse the list and remove matching nodes
-        while current_node and current_node.get_next_node():
-            next_node = current_node.get_next_node()
-            if next_node.get_value() == value_to_remove:
-                # if the next node's value matches the value to remove
-                # update the link of the current node to the next node
-                # but still using the same current node as the baseline
-                # check, because there might be subsequent next nodes to be removed.
-                current_node.set_next_node(next_node.get_next_node())
-            else:
-                # if it doesn't match, we can promote the next node as a valid node
-                # and move on with the check using the next node as the new current
-                # node as the baseline.
-                current_node = current_node.get_next_node()
+            previous_node = current_node
+            current_node = current_node.next
 
-linked_list = LinkedList()
-linked_list.insert_beginning(100)
-linked_list.insert_beginning(20)
-linked_list.insert_beginning(30)
-linked_list.insert_beginning(100)
-linked_list.insert_beginning(100)
-linked_list.insert_beginning(30)
-linked_list.insert_beginning(100)
+    def pop_first(self):
+        original_head = self.head
+        if self.length <= 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            if original_head:
+                return original_head
+            return None
+        self.head = self.head.next
+        original_head.next = None
+        self.length -= 1
+        return original_head
 
-print(linked_list.stringify_list())
+    def get(self, index):
+        counter = 0
+        candidate = self.head
+        while candidate and counter <= index:
+            if counter == index:
+                return candidate
+            candidate = candidate.next
+            counter += 1
+        return None
 
-# linked_list.remove_node(linked_list.get_head_node().get_value())
-linked_list.remove_all_matching_nodes(100)
+    def set_value(self, index, value):
+        candidate = self.get(index)
+        if candidate:
+            candidate.value = value
+            return True
+        return False
 
-print(linked_list.stringify_list())
+    def insert(self, index, value):
+        # index is out of range
+        if index < 0 or index > self.length:
+            return False
+        # index is head
+        if index == 0:
+            return self.prepend(value)
+        # index is tail
+        if index == self.length:
+            return self.append(value)
+
+        previous_node = self.get(index-1)
+        new_node = Node(value)
+        new_node.next = previous_node.next
+        previous_node.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
+
+        previous_node = self.get(index-1)
+        current_node = previous_node.next
+        previous_node.next = current_node.next
+        current_node.next = None
+        self.length -= 1
+
+        return current_node
+
+    def reverse(self):
+        if self.length == 0:
+            return False
+        current_node = self.head
+        new_head = self.tail
+        new_tail = self.head
+
+        # reverse the head and tail pointers.
+        self.head = new_head
+        self.tail = new_tail
+
+        previous_node = None
+
+        for _ in range(self.length):
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+
+        return True
+
+
+linked_list = LinkedList(10)
+print(linked_list.print())
+
+print("\nappend:")
+linked_list.append(12)
+print(linked_list.print())
+
+print("\npop:")
+linked_list.pop()
+print(linked_list.print())
+
+print("\npop it all:")
+linked_list.pop()
+print(linked_list.print())
+
+print("\nprepend:")
+linked_list.prepend(30)
+linked_list.prepend(29)
+linked_list.append(31)
+print(linked_list.print())
+
+print("\npop first:")
+linked_list.pop_first()
+print(linked_list.print())
+
+print("\nget:")
+got = linked_list.get(1)
+if got:
+    print(got.value)
+else:
+    print(got)
+
+print("\nset value:")
+print("got:")
+print(linked_list.set_value(1, 999))
+print("print all:")
+print(linked_list.print())
+
+print("\ninsert:")
+print(linked_list.insert(1, 111))
+print("print all:")
+print(linked_list.print())
+
+print("\nremove:")
+print(linked_list.remove(1))
+print("print all:")
+print(linked_list.print())
+
+print("\nreverse:")
+print(linked_list.reverse())
+print("print all:")
+print(linked_list.print())
