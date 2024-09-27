717a348ba5e22ad850a1910ceda6d59106f840c9
Anderson L. Sergio <asergio@wiley.com>
2024-09-27 11:43:18 -0300

    DLL: add classes and initial methods implementation

---
+++ linked-lists/doubly_linked_lists.py
@@ -0,0 +1,193 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print(self):
+        temp = self.head
+        if not temp:
+            print("List is empty!")
+            return
+        while temp:
+            print("++++++++++++++++++++++++++++++++++++++++++++" +
+                  "\nvalue: "+ str(temp.value) +
+                  "\nnext: "+ str(temp.next)+
+                  "\nprevious: "+ str(temp.prev))
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+        self.length += 1
+        return True
+
+    def pop(self):
+        if not self.head:
+            return None
+
+        node_to_pop = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            # set the new tail to the node that precedes the current tail.
+            self.tail = node_to_pop.prev
+            # disconnect the original tail from the list
+            self.tail.next = None
+            node_to_pop.prev = None
+        self.length -= 1
+        return node_to_pop
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if not self.head:
+            return None
+        node_to_pop = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = node_to_pop.next
+            self.head.prev = None
+            node_to_pop.next = None
+        self.length -= 1
+        return node_to_pop
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index < self.length / 2:
+            candidate = self.head
+            for _ in range(index):
+                candidate = candidate.next
+        else:
+            candidate = self.tail
+            # get the number of hops necessary to hit
+            # the node starting from the tail moving backwards.
+            # (self.length - 1) because length doesn't count from 0 as the index does
+            # so we need to balance the difference of the 2 units.
+            hops = (self.length - 1) - index
+            for _ in range(hops):
+                candidate = candidate.prev
+        return candidate
+
+    def set_value(self, index, value):
+        node_to_be_set = self.get(index)
+        if not node_to_be_set:
+            return False
+        node_to_be_set.value = value
+        return True
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        node_before = self.get(index -1)
+        node_after = node_before.next
+
+        new_node.next = node_after
+        new_node.prev = node_before
+        node_before.next = new_node
+        node_after.prev = new_node
+
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length -1:
+            return self.pop()
+
+        node_to_remove = self.get(index)
+        node_before = node_to_remove.prev
+        node_after = node_to_remove.next
+
+        # update the surrounding nodes' links
+        node_before.next = node_after
+        node_after.prev = node_before
+
+        # disconnect the node from the list
+        node_to_remove.next = None
+        node_to_remove.prev = None
+
+        self.length -= 1
+        return node_to_remove
+
+linked_list = DoublyLinkedList(1)
+print(linked_list.print())
+
+print("\nappend:")
+linked_list.append(2)
+linked_list.append(3)
+print(linked_list.print())
+
+print("\npop:")
+linked_list.pop()
+print(linked_list.print())
+
+print("\nprepend:")
+linked_list.prepend(-1)
+linked_list.print()
+
+print("\npop first:")
+linked_list.pop_first()
+linked_list.print()
+
+print("\nget:")
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
+print(linked_list.get(3).value)
+
+print("\nset value:")
+print("\noriginal list:")
+linked_list.print()
+linked_list.set_value(3, 555)
+print("\nupdated list:")
+linked_list.print()
+linked_list.set_value(3, 4)
+
+print("\ninsert:")
+print(linked_list.insert(4, 111))
+print("print all:")
+print(linked_list.print())
+
+print("\nremove:")
+print(linked_list.remove(4))
+print("print all:")
+print(linked_list.print())
