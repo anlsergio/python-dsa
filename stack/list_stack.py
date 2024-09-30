757f3cd9bef5329a2a9996d84e2366223f126995
Anderson L. Sergio <asergio@wiley.com>
2024-09-30 20:57:10 -0300

    Stack: add is balanced parenthesis algorithm

--- stack/list_stack.py
+++ stack/list_stack.py
@@ -27,3 +27,27 @@ stack.push(2)
 print(stack.pop(), "\n")
 print(stack.pop(), "\n")
 stack.print()
+
+##################################
+# Balanced Parenthesis Algorithm
+##################################
+
+def is_balanced_parentheses(ss):
+    p_stack = Stack()
+    for s in ss:
+        # if it's an opening parenthesis, push it to the stack.
+        if s == '(':
+            p_stack.push(s)
+        # if it's a closing one, there must be a  valid opening parenthesis
+        # on the stack corresponding to the closing counterpart.
+        elif s == ')':
+            if p_stack.is_empty() or p_stack.pop() != '(':
+                return False
+
+    # at the end of the iteration, all corresponding opening
+    # parenthesis added to the stack must be popped out by
+    # their closing counterparts.
+    return p_stack.is_empty()
+
+print("is balanced (False): ", is_balanced_parentheses("((())"))
+print("is balanced (True): ", is_balanced_parentheses("((()))"))
