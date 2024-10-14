a7e2736a199377807376e6db736609aa22fd3a27
Anderson L. Sergio <asergio@wiley.com>
2024-10-14 11:30:23 -0300

    Add MaxHeap implementation

---
+++ heap/max_heap.py
@@ -0,0 +1,94 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current_index = len(self.heap) - 1
+
+        while current_index > 0 and self.heap[current_index] > self.heap[self._parent(current_index)]:
+            self._swap(current_index, self._parent(current_index))
+            current_index = self._parent(current_index)
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        max_value = self.heap[0]
+        # move the last item of the heap to the top.
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return max_value
+
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            # for each iteration, if the corresponding left and right indexes are not out of bounds,
+            # find the greatest value amongst the left and right children.
+            if (left_index < len(self.heap) and
+                    self.heap[left_index] > self.heap[max_index]):
+                max_index = left_index
+            if (right_index < len(self.heap) and
+                    self.heap[right_index] > self.heap[max_index]):
+                max_index = right_index
+            # if the greatest value is not pointing at the index
+            # they need to switch places.
+            if max_index != index:
+                self._swap(index, max_index)
+                # move the cursor down to the new greatest value index.
+                index = max_index
+            else:
+                # if the cursor is pointing to the greatest value, there's nothing else to do.
+                # This means, that the value is sunk down to the right place already and the heap is valid.
+                return
+
+
+my_heap = MaxHeap()
+my_heap.insert(99)
+my_heap.insert(72)
+my_heap.insert(61)
+my_heap.insert(58)
+
+print(my_heap.heap)
+
+my_heap.insert(100)
+
+print(my_heap.heap)
+
+my_heap.insert(75)
+
+print(my_heap.heap)
+
+print("\nRemove:")
+my_heap = MaxHeap()
+my_heap.insert(95)
+my_heap.insert(75)
+my_heap.insert(80)
+my_heap.insert(55)
+my_heap.insert(60)
+my_heap.insert(50)
+my_heap.insert(65)
+
+print(my_heap.heap)
+
+my_heap.remove()
+print(my_heap.heap)
+
+my_heap.remove()
+print(my_heap.heap)
