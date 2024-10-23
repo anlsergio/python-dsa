b2bb65aea1fb6f3717a22ff2b151d1fbb14e5770
Anderson L. Sergio <anderson.als04@gmail.com>
2024-10-23 15:09:19 -0300

    List: remove duplicates in-place algorithm

--- list/list.py
+++ list/list.py
@@ -71,3 +71,65 @@ def find_longest_string(string_list):
 string_list = ['apple', 'banana', 'kiwi', 'pear']
 longest = find_longest_string(string_list)
 print(longest)
+
+
+# Given a sorted list of integers, rearrange the list in-place such that
+# all unique elements appear at the beginning of the list.
+#
+# Your function should return the new length of the list containing only unique elements.
+# Note that you should not create a new list or use any additional
+# data structures to solve this problem.
+# The original list should be modified in-place.
+
+# Constraints:
+#
+# - The input list is sorted in ascending order.
+# - The input list may contain duplicates.
+# - The function should have a time complexity of O(n), where n is the length of the input list.
+# - The function should have a space complexity of O(1), i.e., it should not use any
+# additional data structures or create new lists
+# (this also means you cannot use a set like we did earlier in the course).
+
+def remove_duplicates(nums):
+    move_count = 0
+    # there's no need of iterating through the last item,
+    # because it's included in the i+1 validation.
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            to_move = nums.pop(i)
+            nums.append(to_move)
+            move_count += 1
+    return len(nums) - move_count
+
+
+# Test case 1: Empty list
+test1 = []
+print(f"Test 1 Before: {test1}")
+result1 = remove_duplicates(test1)
+print(f"Test 1 After: {test1[:result1]}")
+print(f"New Length: {result1}")
+print("------")
+
+# Test case 2: List with all duplicates
+test2 = [1, 1, 1, 1, 1]
+print(f"Test 2 Before: {test2}")
+result2 = remove_duplicates(test2)
+print(f"Test 2 After: {test2[:result2]}")
+print(f"New Length: {result2}")
+print("------")
+
+# Test case 3: List with no duplicates
+test3 = [1, 2, 3, 4, 5]
+print(f"Test 3 Before: {test3}")
+result3 = remove_duplicates(test3)
+print(f"Test 3 After: {test3[:result3]}")
+print(f"New Length: {result3}")
+print("------")
+
+# Test case 4: List with some duplicates
+test4 = [1, 1, 2, 2, 3, 4, 5, 5]
+print(f"Test 4 Before: {test4}")
+result4 = remove_duplicates(test4)
+print(f"Test 4 After: {test4[:result4]}")
+print(f"New Length: {result4}")
+print("------")
